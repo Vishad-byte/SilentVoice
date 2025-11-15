5d34fa76906feb866589092e83079c946b7e6ec1
Vishad-byte <srivastavavishad0212@gmail.com>
2025-11-15 22:36:52 +0530

    feat: add new UI components for message handling and Dashboard and update user message settings

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -48,7 +48,7 @@ export const authOptions: NextAuthOptions = {
       if (user) {               //this is done to inject data in our token such that our database calls can be reduced and we can access our required data from token and the data inside it
         token._id = user._id?.toString(); // Convert ObjectId to string
         token.isVerified = user.isVerified;
-        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.isAcceptingMessage = user.isAcceptingMessage;
         token.username = user.username;
       }
       return token;
@@ -57,7 +57,7 @@ export const authOptions: NextAuthOptions = {
       if (token) {
         session.user._id = token._id;
         session.user.isVerified = token.isVerified;
-        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.isAcceptingMessage = token.isAcceptingMessage;
         session.user.username = token.username;
       }
       return session;
--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -3,7 +3,7 @@ import { Message } from "@/model/User";
 export interface ApiResponse{
     success: boolean;
     message: string;
-    isAcceptingMessages?: boolean;
+    isAcceptingMessage?: boolean;
     messages?: Array<Message>
     // Optional debug/meta information (used during dev to surface external service responses)
     meta?: any   //TODO: remove this meta before deployment
